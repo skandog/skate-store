923041929a4581615ad17eb24d753717d0ee38d2
skandog <muumrar@gmail.com>
2022-11-14 14:34:25 +0000

    set up _app page to handle layout component - no need for chakra

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
 import Layout from '../components/layouts/main'
 
-const Website = ({Component, pageProps, router}) => {
-    return ();
+const Website = ({ Component, pageProps, router }) => {
+  return (
+    <Layout router={router}>
+      <Component {...pageProps} key={router.route} />
+    </Layout>
+  )
 }
- 
-export default Website;
+
+export default Website
