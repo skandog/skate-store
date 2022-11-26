e38dd77d7eb2df6223dd3307e6d96c6d810cfca6
skandog <muumrar@gmail.com>
2022-11-26 13:06:09 +0000

    added menu links into html, needs style

--- components/nav.js
+++ components/nav.js
@@ -16,65 +16,127 @@ const Nav = props => {
 
   return (
     <nav {...props}>
-      <div className="nav-top">
-        <div className="nav-item">
-          <Image
-            src="/img/oster-944x942.png"
-            alt="logo"
-            width="64"
-            height="64"
-          />
-        </div>
-        <div className="nav-item">
-          <h1>Skate Store</h1>
-        </div>
-        <div className="nav-item">
-          <div className="search">
-            <input
-              type="text"
-              placeholder="Search..."
-              className="search-input"
-            ></input>
-            <FaSearch className="search-icon" />
+      <div className="nav-web">
+        <div className="nav-top">
+          <div className="nav-item">
+            <Image
+              src="/img/oster-944x942.png"
+              alt="logo"
+              width="64"
+              height="64"
+            />
+          </div>
+          <div className="nav-item">
+            <h1>Skate Store</h1>
+          </div>
+          <div className="nav-item">
+            <div className="search">
+              <input
+                type="text"
+                placeholder="Search..."
+                className="search-input"
+              ></input>
+              <FaSearch className="search-icon" />
+            </div>
           </div>
         </div>
+        <div className="nav-bottom">
+          <LinkItem
+            href="/board-1"
+            path={path}
+            className="nav-menu-item next-link"
+          >
+            Board 1
+          </LinkItem>
+          <LinkItem
+            href="/board-2"
+            path={path}
+            className="nav-menu-item next-link"
+          >
+            Board 2
+          </LinkItem>
+          <LinkItem
+            href="/board-3"
+            path={path}
+            className="nav-menu-item next-link"
+          >
+            Board 3
+          </LinkItem>
+          <LinkItem
+            href="/board-4"
+            path={path}
+            className="nav-menu-item next-link"
+          >
+            Board 4
+          </LinkItem>
+          <LinkItem
+            href="/board-5"
+            path={path}
+            className="nav-menu-item next-link"
+          >
+            Board 5
+          </LinkItem>
+        </div>
       </div>
-      <div className="nav-bottom">
-        <LinkItem
-          href="/board-1"
-          path={path}
-          className="nav-menu-item next-link"
-        >
-          Board 1
-        </LinkItem>
-        <LinkItem
-          href="/board-2"
-          path={path}
-          className="nav-menu-item next-link"
-        >
-          Board 2
-        </LinkItem>
-        <LinkItem
-          href="/board-3"
-          path={path}
-          className="nav-menu-item next-link"
-        >
-          Board 3
-        </LinkItem>
-        <LinkItem
-          href="/board-4"
-          path={path}
-          className="nav-menu-item next-link"
-        >
-          Board 4
-        </LinkItem>
-        <LinkItem
-          href="/board-5"
-          path={path}
-          className="nav-menu-item next-link"
-        >
-          Board 5
-        </LinkItem>
+      <div className="nav-mobile">
+        <div class="navigation">
+          <span id="closebtn" onclick="navToggle()">
+            <span class="line1"></span>
+            <span class="line2"></span>
+            <span class="line3"></span>
+          </span>
+          <div class="logo">
+            <img src="" />
+          </div>
+
+          <ul class="menulist">
+            <li>
+              <LinkItem
+                href="/board-1"
+                path={path}
+                className="mob-menu-item next-link"
+              >
+                Board 1
+              </LinkItem>
+            </li>
+            <li>
+              <LinkItem
+                href="/board-2"
+                path={path}
+                className="mob-menu-item next-link"
+              >
+                Board 2
+              </LinkItem>
+            </li>
+            <li>
+              <LinkItem
+                href="/board-3"
+                path={path}
+                className="mob-menu-item next-link"
+              >
+                Board 3
+              </LinkItem>
+            </li>
+            <li>
+              <LinkItem
+                href="/board-4"
+                path={path}
+                className="mob-menu-item next-link"
+              >
+                Board 4
+              </LinkItem>
+            </li>
+            <li>
+              <LinkItem
+                href="/board-5"
+                path={path}
+                className="mob-menu-item next-link"
+              >
+                Board 5
+              </LinkItem>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   )
