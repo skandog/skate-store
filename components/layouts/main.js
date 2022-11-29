49102cf03afc8315dd325f6217ed494825918198
skandog <muumrar@gmail.com>
2022-11-29 17:34:22 +0000

    added icon to layout component

--- components/layouts/main.js
+++ components/layouts/main.js
@@ -9,6 +9,7 @@ const Main = ({ children, router }) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Skate Store</title>
+        <link rel="icon" type="image/png" href="/img/oster-944x942.png" />
       </Head>
       <Nav path={router.asPath}>
         <div className="container">{children}</div>
