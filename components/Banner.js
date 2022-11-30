b36c66bbb329c01db062cdb7220f8d0c4e67c4fb
skandog <muumrar@gmail.com>
2022-11-30 17:38:18 +0000

    written button to change board in banner

--- components/Banner.js
+++ components/Banner.js
@@ -1,10 +1,15 @@
 import Image from 'next/image'
+import { FaArrowAltCircleRight } from 'react-icons/fa'
 
-const Banner = ({ src }) => {
+const Banner = ({ src, handleClick }) => {
   return (
     <div className="banner">
       <div className="banner-item">
-        <Image className='banner-img' src={src} width={150}/>
+        <Image alt="skate-board" className="banner-img" src={src} width={150} />
+
+        <div className="arrow-button" onClick={handleClick}>
+          <FaArrowAltCircleRight className="banner-arrow" size={50} />
+        </div>
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,24 @@
+import { useState } from 'react'
 import Banner from '../components/Banner'
 import board1 from '../public/img/PngItem_3637665.png'
+import board2 from '../public/img/PngItem_3637674.png'
 const Page = () => {
+  const [currentBoard, setCurrentBoard] = useState(0)
+
+  const boards = [board1, board2]
+
+  const hamdleClick = () => {
+    if (currentBoard < boards.length - 1) {
+      setCurrentBoard(currentBoard + 1)
+    } else {
+      setCurrentBoard(0)
+    }
+  }
+
+  console.log(currentBoard)
   return (
     <div>
-      
-      <Banner src={board1} />
+      <Banner src={boards[currentBoard]} handleClick={hamdleClick} />
     </div>
   )
 }
