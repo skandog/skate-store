7c07c74ca0eefb85c0c026ea2b390ab1856bf2f9
skandog <muumrar@gmail.com>
2022-11-30 17:40:36 +0000

    added all the boards but not super happy with the behaviour

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,14 @@ import { useState } from 'react'
 import Banner from '../components/Banner'
 import board1 from '../public/img/PngItem_3637665.png'
 import board2 from '../public/img/PngItem_3637674.png'
+import board3 from '../public/img/PngItem_3637754.png'
+import board4 from '../public/img/PngItem_5645814.png'
+import board5 from '../public/img/PngItem_6562957.png'
+
 const Page = () => {
   const [currentBoard, setCurrentBoard] = useState(0)
 
-  const boards = [board1, board2]
+  const boards = [board1, board2, board3, board4, board5]
 
   const hamdleClick = () => {
     if (currentBoard < boards.length - 1) {
