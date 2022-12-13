fe5592d53743760dab466e9bd0e85484ed6e53ca
skandog <muumrar@gmail.com>
2022-12-13 15:23:24 +0000

    rendering image on product page

--- pages/boards/bape.js
+++ pages/boards/bape.js
@@ -1,5 +1,17 @@
+import products from '../../public/products'
+import Image from 'next/image'
+
 const Bape = () => {
-  return <div className="board-page-container">Bape</div>
+  return (
+    <div className="board-page-container">
+      <Image
+        alt="skate-board"
+        className="product-page-img"
+        src={products[0].img}
+        width={150}
+      />
+    </div>
+  )
 }
 
 export default Bape
