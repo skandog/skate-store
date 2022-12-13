7df1146c200f48086949ecba141e1eb97f2d8d74
skandog <muumrar@gmail.com>
2022-12-13 10:51:28 +0000

    added url path to class

--- public/products.js
+++ public/products.js
@@ -5,17 +5,18 @@ import blind from '../public/img/PngItem_5645814.png'
 import antiHero from '../public/img/PngItem_6562957.png'
 
 class SkateBoard {
-  constructor(name, img) {
+  constructor(name, img, path) {
     this.name = name
     this.img = img
+    this.path = path
   }
 }
 
-let Bape = new SkateBoard('Bape', bape)
-let DeathWish = new SkateBoard('DeathWish', deathwish)
-let Splash = new SkateBoard('Splash', splash)
-let Blind = new SkateBoard('Blind', blind)
-let AntiHero = new SkateBoard('AntiHero', antiHero)
+let Bape = new SkateBoard('Bape', bape, '/bape')
+let DeathWish = new SkateBoard('DeathWish', deathwish, '/deathwish')
+let Splash = new SkateBoard('Splash', splash, '/splash')
+let Blind = new SkateBoard('Blind', blind, '/blind')
+let AntiHero = new SkateBoard('AntiHero', antiHero, '/antihero')
 const products = [Bape, DeathWish, Splash, Blind, AntiHero]
 
 export default products
