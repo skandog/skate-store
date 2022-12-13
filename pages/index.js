f3a5e96134f1105024229b77c5f57ba3a0d57a39
skandog <muumrar@gmail.com>
2022-12-13 10:02:46 +0000

    added carousel functionality to banner - want to refactor and componentise now

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Banner from '../components/Banner'
 import board1 from '../public/img/PngItem_3637665.png'
 import board2 from '../public/img/PngItem_3637674.png'
@@ -11,18 +11,42 @@ const Page = () => {
 
   const boards = [board1, board2, board3, board4, board5]
 
-  const hamdleClick = () => {
-    if (currentBoard < boards.length - 1) {
-      setCurrentBoard(currentBoard + 1)
+  const [count, setCount] = useState(0)
+  const [mousedOver, setMousedOver] = useState(false)
+
+  useEffect(() => {
+    // set an interval timer if we are currently moused over
+    if (mousedOver) {
+      const timer = setInterval(() => {
+        // cycle prevCount using mod instead of checking for hard-coded length
+        setCount(prevCount => (prevCount + 1) % boards.length)
+      }, 2000)
+      // automatically clear timer the next time this effect is fired or
+      // the component is unmounted
+      return () => clearInterval(timer)
     } else {
-      setCurrentBoard(0)
+      // otherwise (not moused over), reset the counter
+      setCount(0)
+    }
+    // the dependency on mousedOver means that this effect is fired
+    // every time mousedOver changes
+  }, [mousedOver])
+
+  const handleClick = () => {
+    if (count < boards.length - 1) {
+      setCount(count + 1)
+    } else {
+      setCount(0)
     }
   }
 
-  console.log(currentBoard)
+  console.log(count)
   return (
-    <div>
-      <Banner src={boards[currentBoard]} handleClick={hamdleClick} />
+    <div
+      onMouseOver={() => setMousedOver(true)}
+      onMouseOut={() => setMousedOver(false)}
+    >
+      <Banner src={boards[count]} handleClick={handleClick} />
     </div>
   )
 }
