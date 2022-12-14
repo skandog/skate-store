71197f30fd1aa3ed5bc0903ef1c4aa7af21b6608
skandog <muumrar@gmail.com>
2022-12-14 15:02:15 +0000

    added to the product page - still very much wip

--- pages/boards/bape.js
+++ pages/boards/bape.js
@@ -5,12 +5,21 @@ const Bape = () => {
   return (
     <div className="board-page-container">
       <h1>{products[0].name} Board</h1>
-      <Image
-        alt="skate-board"
-        className="product-page-img"
-        src={products[0].img}
-        width={150}
-      />
+      <div className="product-info">
+        <Image
+          alt="skate-board"
+          className="product-page-img"
+          src={products[0].img}
+          width={150}
+        />
+        <p>
+          Check out BAPE's new skateboard deck! With its sleek design and trendy
+          camo print, this deck is perfect for cruising the streets in style.
+          The durable construction ensures a smooth ride, while the BAPE
+          branding adds a touch of streetwear flair. Get yours today and take
+          your skating to the next level.
+        </p>
+      </div>
     </div>
   )
 }
